5d2c50244b6d8b3052ae0805798238077f543f5c
Scryotic <81018435+scryotic-tech@users.noreply.github.com>
2022-11-22 11:52:49 +0000

    Update starknet.sh

--- tStarknet/starknet.sh
+++ tStarknet/starknet.sh
@@ -14,14 +14,14 @@ echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 sleep 2
 
 if [ ! $ApiURL ]; then
-	read -p "Masukin API URLmu: " ApiURL
-	echo 'export Address='$ApiURL >> $HOME/.bash_profile
+	read -p "Masukin URL HTTPSmu: " ApiURL
+	echo 'export ApiURL='$ApiURL >> $HOME/.bash_profile
 fi
 
 source $HOME/.bash_profile
 
 echo '================================================='
-echo -e "API URL: \e[1m\e[32m$ApiURL\e[0m"
+echo -e "URL HTTPS: \e[1m\e[32m$ApiURL\e[0m"
 echo '================================================='
 
 sleep 2
@@ -29,16 +29,16 @@ sleep 2
 echo -e "\e[1m\e[32m1. Installing Docker... \e[0m" && sleep 1
 sudo apt update -y && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt install docker-ce
 
-echo -e "\e[1m\e[32m1. Installing Essential package... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m2. Installing Essential package... \e[0m" && sleep 1
 sudo apt-get install -y pkg-config curl git build-essential libssl-dev screen
 
-echo -e "\e[1m\e[32m1. Cloning pathfinder... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m3. Cloning pathfinder... \e[0m" && sleep 1
 git clone --branch v0.4.0 https://github.com/eqlabs/pathfinder.git
 
-echo -e "\e[1m\e[32m1. Creating pathfinder folder... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m4. Creating pathfinder folder... \e[0m" && sleep 1
 mkdir -p $HOME/pathfinder
 
-echo -e "\e[1m\e[32m1. Running Docker... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m5. Running Docker... \e[0m" && sleep 1
 docker run -d \
   --rm \
   -p 9545:9545 \
