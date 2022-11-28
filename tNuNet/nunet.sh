1235bbd5db0f284378cfa43cf5501e102fe65775
Scryotic <81018435+scryotic-tech@users.noreply.github.com>
2022-11-28 11:19:12 +0000

    Update nunet.sh

--- tNuNet/nunet.sh
+++ tNuNet/nunet.sh
@@ -15,12 +15,12 @@ sleep 2
 
 if [ ! $nAddress ]; then
 	read -p "Input your 0x Address: " nAddress
-	echo 'export Address='$nAddress >> $HOME/.bash_profile
+	echo 'export nAddress='$nAddress >> $HOME/.bash_profile
 fi
 source $HOME/.bash_profile
 if [ ! $nPrivateKey ]; then
 	read -p "Input your Private Key: " nPrivateKey
-	echo 'export PrivateKey='$nPrivateKey >> $HOME/.bash_profile
+	echo 'export nPrivateKey='$nPrivateKey >> $HOME/.bash_profile
 fi
 source $HOME/.bash_profile
 
@@ -37,4 +37,26 @@ cd $home && wget https://d.nunet.io/nunet-dms-latest.deb
 echo -e "\e[1m\e[32m2. Installing DMS... \e[0m" && sleep 1
 sudo apt update && sudo apt install ./nunet-dms-latest.deb
 
+echo -e "e[1m\e[32m3. DMS status... \e[0m" && sleep 1
+ps aux |grep nunet-dms
+
+sleep 1
+echo -e "e[1m/e[32m4. Checking your Memory & CPU... \e[0m" && sleep 1
+nunet available --pretty
+
+sleep 2
+
+if [ ! $nMem ]; then
+	read -p "Input your Memory: " nMem
+	echo 'export nMem='$nMem >> $HOME/.bash_profile
+fi
+source $HOME/.bash_profile
+if [ ! $nPrivateKey ]; then
+	read -p "Input your CPU: " nCPU
+	echo 'export nCPU='$nCPU >> $HOME/.bash_profile
+fi
+source $HOME/.bash_profile
+
+echo -e "e[1m\e[32m4. Nunet Onboarding... \e[0m" && sleep 1
+nunet onboard -m $CPU -c $Mem -n nunet-test -a $nAddress
 
