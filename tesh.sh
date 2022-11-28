830b6aa8c2060d550369cb037cda9beed781e561
Scryotic <81018435+scryotic-tech@users.noreply.github.com>
2022-11-28 16:55:58 +0000

    Update tesh.sh

--- tesh.sh
+++ tesh.sh
@@ -30,33 +30,26 @@ OPTIONS="Yes No"
 
 select opt in $OPTIONS; do
     if [ "$opt" = "Yes" ]; then 
-        if [ ! $nAddress ]; then
-        read -p "Input your 0x Address: " nAddress
-        echo 'export nAddress='$nAddress >> $HOME/.bash_profile
-        fi
-        source $HOME/.bash_profile
-        if [ ! $nPrivateKey ]; then
-        read -p "Input your Private Key: " nPrivateKey
-        echo 'export nPrivateKey='$nPrivateKey >> $HOME/.bash_profile
         break
-	fi
     elif [ "$opt" = "No" ]; then
-    	echo -e "\e[1m\e[32m Creating New Wallet...  \e[0m" && sleep 1
+    	echo -e "\e[1m\e[32m5. Creating New Wallet...  \e[0m" && sleep 1
         nunet wallet new
-        if [ ! $nAddress ]; then
-        read -p "Input your 0x Address: " nAddress
-        echo 'export nAddress='$nAddress >> $HOME/.bash_profile
-        fi
-        source $HOME/.bash_profile
-        if [ ! $nPrivateKey ]; then
-        read -p "Input your Private Key: " nPrivateKey
-        echo 'export nPrivateKey='$nPrivateKey >> $HOME/.bash_profile
 	break
-	fi
     fi
 done
         
 sleep 2
+
+if [ ! $nAddress ]; then
+	read -p "Input your 0x Address: " nAddress
+	echo 'export nAddress='$nAddress >> $HOME/.bash_profile
+fi
+source $HOME/.bash_profile
+if [ ! $nPrivateKey ]; then
+	read -p "Input your Private Key: " nPrivateKey
+	echo 'export nPrivateKey='$nPrivateKey >> $HOME/.bash_profile
+fi
+source $HOME/.bash_profile
 
 echo '================================================='
 echo -e "Your Address: \e[1m\e[32m$nAddress\e[0m"
@@ -67,7 +60,7 @@ sleep 2
 
 source $HOME/.bash_profile
 
-echo -e "\e[1m\e[32m5. Checking your Memory & CPU... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m6. Checking your Memory & CPU... \e[0m" && sleep 1
 nunet available --pretty
 
 sleep 2
@@ -84,9 +77,9 @@ fi
 source $HOME/.bash_profile
 
 sleep 1
-echo -e "\e[1m\e[32m6. Deleting trash... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m7. Deleting trash... \e[0m" && sleep 1
 rm nunet.sh
 rm nunet-dms-latest.deb
 
-echo -e "\e[1m\e[32m7. Nunet Onboarding... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m8. Nunet Onboarding... \e[0m" && sleep 1
 nunet onboard -m $nMem -c $nCPU -n nunet-test -a $nAddress
