6c701aae4526c402caf2e5a763410ef330bcba78
Scryotic <81018435+scryotic-tech@users.noreply.github.com>
2022-12-09 14:22:01 +0000

    Update tfleek.sh

--- tFleek/tfleek.sh
+++ tFleek/tfleek.sh
@@ -14,17 +14,19 @@ echo "+++++++++++++++++++++++++++++++++++++++++++++++++++"
 sleep 2
 
 echo -e "\e[1m\e[32m1. Installing Dependencies... \e[0m" && sleep 1
-sudo apt-get update && apt-get upgrade && sudo apt-get install build-essential cmake curl protobuf-compiler clang pkg-config libssl-dev git ca-certificates gnupg lsb-release -y && sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt-get update && apt-get upgrade && sudo apt-get install build-essential cmake curl protobuf-compiler clang pkg-config libssl-dev git ca-certificates gnupg lsb-release -y && sudo mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 cargo install sccache
 
 echo -e "\e[1m\e[32m2. Installing URSA... \e[0m" && sleep 1
 git clone https://github.com/fleek-network/ursa.git && cd ursa && rm Makefile
+wget -q https://raw.githubusercontent.com/scryotic-tech/Nodetestnet/main/tFleek/Makefile && make install
+export DOCKER_BUILDKIT=1 && make docker-build
+
+echo -e "\e[1m\e[32m3. Running URSA-Docker... \e[0m" && sleep 1
+make docker-run
 
